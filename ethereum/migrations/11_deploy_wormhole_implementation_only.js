9cdc1c6aace7859d6f984d65a543b27be6d4e458
Bruce Riley <briley@jumptrading.com>
2022-10-13 13:34:23 +0000

    Undo deploy script changes

--- ethereum/migrations/11_deploy_wormhole_implementation_only.js
+++
@@ -1,6 +0,0 @@
-// MNEMONIC="" npm run deploy-wormhole-implementation-only -- --network acala
-
-const Implementation = artifacts.require("Implementation");
-module.exports = async function(deployer) {
-  await deployer.deploy(Implementation);
-};
--- ethereum/migrations/12_deploy_nft_bridge_implementation_only.js
+++
@@ -1,7 +0,0 @@
-// MNEMONIC="" npm run deploy-wormhole-implementation-only -- --network acala
-
-const BridgeImplementation = artifacts.require("NFTBridgeImplementation");
-module.exports = async function(deployer, network) {
-  if (network === "test") return;
-  await deployer.deploy(BridgeImplementation);
-};
